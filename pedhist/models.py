5a42f81955a60e0ee062c6ede06380e713af1174
Heitor Chang <heitorchang@gmail.com>
2018-11-19 16:13:30 -0200

    price ok

--- pedhist/models.py
+++ pedhist/models.py
@@ -10,8 +10,13 @@ class Pedido(models.Model):
     cond = models.CharField(max_length=80)
     desc = models.CharField(max_length=4)
     produtos = models.TextField()
+    price_ok = models.BooleanField()
     obs1 = models.CharField(max_length=80, blank=True)
     obs2 = models.TextField(blank=True)
 
     def __str__(self):
-        return "{} {} {}".format(self.created, self.cliente.business_name, self.produtos)
+        if self.price_ok:
+            p_msg = "Price OK"
+        else:
+            p_msg = "Price NOT checked"
+        return "[{}] {} : {} ({})".format(self.created, self.cliente.business_name.title(), self.produtos, p_msg)
